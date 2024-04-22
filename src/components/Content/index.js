7e4aa79a295e7926730f0359f1c8c059de08283d
Hau Trinh Van <trinhvanhau2003@gmail.com>
2024-04-22 16:36:13 +0700

    Change component Content

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -2,7 +2,10 @@ import styles from './content.module.css'
 
 function Content() {
     return (
-        <p className={styles.paragraph}> Toi la Trinh Van Hau day ne!</p>
+        <>
+            <p>Test nhanh dev!!!</p>
+            <p className={styles.paragraph}> Toi la Trinh Van Hau day ne!</p>
+        </>
     )
 }
 
